cb2eb7d6f842f4a9afa478a53cfb024ab2b8b93d
Pranay Alamalakala <pranay.a4795@gmail.com>
2023-11-13 00:32:22 +0000

    intialize project

---
+++ src/test/java/pranay/springframework/sfgpetclinicstart/SfgPetClinicStartApplicationTests.java
@@ -0,0 +1,13 @@
+package pranay.springframework.sfgpetclinicstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
