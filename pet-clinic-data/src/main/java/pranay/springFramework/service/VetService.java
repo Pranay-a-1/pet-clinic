dca87c04895574e65b4c7af24c64030d588ba31e
Pranay Alamalakala <pranay.a4795@gmail.com>
2023-11-25 17:48:12 +0000

    add services . closes #5

---
+++ pet-clinic-data/src/main/java/pranay/springFramework/service/VetService.java
@@ -0,0 +1,14 @@
+package pranay.springFramework.service;
+
+import pranay.springFramework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
