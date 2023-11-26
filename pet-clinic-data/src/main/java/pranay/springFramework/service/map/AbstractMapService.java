1d46dd880089a9526bae45f9210f23f0da9be9b1
Pranay Alamalakala <pranay.a4795@gmail.com>
2023-11-26 18:23:56 +0000

    adding bootstrap for data . closes #14

--- pet-clinic-data/src/main/java/pranay/springFramework/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/pranay/springFramework/service/map/AbstractMapService.java
@@ -1,12 +1,13 @@
 package pranay.springFramework.service.map;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractMapService<T, ID> {
 
-    protected Map<ID, T> map;
+    protected Map<ID, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
