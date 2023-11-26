b3313a6967ef9ba29e3eb9e659de451caec075c0
Pranay Alamalakala <pranay.a4795@gmail.com>
2023-11-26 12:52:43 +0000

    add map service . closes #6

--- pet-clinic-data/src/main/java/pranay/springFramework/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pranay/springFramework/model/BaseEntity.java
@@ -1,7 +1,14 @@
 package pranay.springFramework.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity implements Serializable {
 
     private Long id;
---
+++ pet-clinic-data/src/main/java/pranay/springFramework/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package pranay.springFramework.service.map;
+
+import pranay.springFramework.model.Pet;
+import pranay.springFramework.service.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
